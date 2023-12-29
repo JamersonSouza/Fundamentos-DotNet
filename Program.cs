258a56ab465bc1c95479d9d327beaa334478b2fe
jamersonsouza <jamersonsouza@outlook.com>
2023-12-28 23:55:08 -0300

    Instanciando o objeto de estacionamento na classe principal

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using projeto_estacionamento.model;
+
+// Coloca o encoding para UTF8 para exibir acentuação
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+decimal precoInicial = 0;
+decimal precoPorHora = 0;
+
+Estacionamento estacionamento = new Estacionamento(precoInicial, precoPorHora);
