742f983f976de6fddf7784c04844ce23a7c995a0
jamersonsouza <jamersonsouza@outlook.com>
2023-12-29 22:27:18 -0300

    Método para adicionar veículo + validação

--- model/Estacionamento.cs
+++ model/Estacionamento.cs
@@ -20,7 +20,61 @@ namespace projeto_estacionamento.model
          public void AdicionarVeiculo()
         {
             // TODO
+            string placas;
             Console.WriteLine("Digite a placa do veículo para estacionar:");
+            placas = Console.ReadLine() ?? string.Empty;
+            if(!string.IsNullOrWhiteSpace(placas)){
+                veiculos.Add(placas);
+                Console.WriteLine("Veículo adicionado com sucesso!");
+            }
+            else{
+                Console.WriteLine("Insira as informações de um veículo corretamente.");
+            }  
+        }
+
+    public void ListarVeiculos()
+        {
+            // Verifica se há veículos no estacionamento
+            if (veiculos.Any())
+            {
+                Console.WriteLine("Os veículos estacionados são:");
+                // TODO: Realizar um laço de repetição, exibindo os veículos estacionados
+                // *IMPLEMENTE AQUI*
+            }
+            else
+            {
+                Console.WriteLine("Não há veículos estacionados.");
+            }
+        }
+
+          public void RemoverVeiculo()
+        {
+            Console.WriteLine("Digite a placa do veículo para remover:");
+
+            // Pedir para o usuário digitar a placa e armazenar na variável placa
+            // *IMPLEMENTE AQUI*
+            string placa = "";
+
+            // Verifica se o veículo existe
+            if (veiculos.Any(x => x.ToUpper() == placa.ToUpper()))
+            {
+                Console.WriteLine("Digite a quantidade de horas que o veículo permaneceu estacionado:");
+
+                // TODO: Pedir para o usuário digitar a quantidade de horas que o veículo permaneceu estacionado,
+                // TODO: Realizar o seguinte cálculo: "precoInicial + precoPorHora * horas" para a variável valorTotal                
+                // *IMPLEMENTE AQUI*
+                int horas = 0;
+                decimal valorTotal = 0; 
+
+                // TODO: Remover a placa digitada da lista de veículos
+                // *IMPLEMENTE AQUI*
+
+                Console.WriteLine($"O veículo {placa} foi removido e o preço total foi de: R$ {valorTotal}");
+            }
+            else
+            {
+                Console.WriteLine("Desculpe, esse veículo não está estacionado aqui. Confira se digitou a placa corretamente");
+            }
         }
 
     }
